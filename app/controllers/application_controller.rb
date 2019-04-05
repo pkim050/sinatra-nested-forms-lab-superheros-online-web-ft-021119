0225c353cfcc4763656e205920c152f17b8f9e07
Patrick Kim <pkim050@gmail.com>
2019-04-05 23:42:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
     post '/teams' do
       #@team = Team.new(params[:team])
-      #binding.pry
+      binding.pry
       #params[:team][:members].each {|element| Hero.new(element)}
       #@members = Hero.all
 
